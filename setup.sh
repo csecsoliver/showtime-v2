c1f415494a3bad3d543f83b880fc12b146377a5d
Csécs Olivér Elemér <csecs.oliver.elemer@students.jedlik.eu>
2025-12-08 00:23:24 +0000

    setup script optimization

--- setup.sh
+++ setup.sh
@@ -2,11 +2,8 @@ sudo apt-get -y install --no-install-recommends wget gnupg ca-certificates lsb-r
 wget -O - https://openresty.org/package/pubkey.gpg | sudo gpg --dearmor -o /usr/share/keyrings/openresty.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/openresty.gpg] http://openresty.org/package/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list > /dev/null
 sudo apt-get update
-sudo apt-get -y install openresty
-sudo apt install -y uuid-dev
-sudo apt-get install libmagickwand-dev -y
-sudo apt install luarocks -y
-sudo apt install libcurl4-openssl-dev -y
+sudo apt-get install openresty uuid-dev libmagickwand-dev luarocks libcurl4-openssl-dev libargon2-dev libssl-dev libsqlite3-dev -y
+
 sudo luarocks install lapis
 sudo luarocks install moonscript
 sudo luarocks install markdown
